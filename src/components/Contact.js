351da8f6defcf6aaea51c5c5515d74f071c79de7
Ayo <Ayo.meshe@gmail.com>
2023-07-20 23:16:36 +0100

    update commit

---
+++ src/components/Contact.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import { Container } from "react-bootstrap";
+
+const Contact = () => {
+  const formInitialDetails = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    message: "",
+  };
+
+  const [formDetails, setFormDetails] = useState(formInitialDetails);
+  const [buttonText, setButtonText] = useState("Send");
+  const [status, setStatus] = useState({});
+
+  return {};
+};
+
+export default Contact;
---
+++ src/components/Skills.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const skills = () => {
+  return;
+};
+
+export default skills;

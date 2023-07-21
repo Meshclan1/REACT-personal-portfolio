74dfd6b2bb24c28a40f675961d1e2b92507c5820
Ayo <Ayo.meshe@gmail.com>
2023-07-21 18:06:22 +0100

    Banner

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./App.css";
 import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import NavBar from "./components/NavBar";
+import Banner from "./components/Banner";
 
 const App = () => {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
     </div>
   );
 };
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/images/header-img.svg";
 
 const Banner = () => {
-  return;
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = ["Full-Stack Engineer"];
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline"> Welcome to my Portfolio</span>
+            <h1>
+              {`👋 Hey there, I'm Ayo a `}
+              <span className="wrap">{text}</span>
+              <h3>Welcome to my profile</h3>
+              <p>
+                I'm a skilled self-taught engineer with a strong focus on
+                JavaScript and a love for creating exceptional web applications.
+                My journey into the world of software development has been
+                fueled by a child-like curiosity and a desire to continously
+                learn new technologies.
+              </p>
+              <button onClick={() => console.log("connect")}>
+                Let’s Connect <ArrowRightCircle size={25} />
+              </button>
+            </h1>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Header Img" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default Banner;

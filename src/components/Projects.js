61b8c4fb8f84162e20b501e22ad393570d6a1411
Ayo Meshe <100096778+Meshclan1@users.noreply.github.com>
2024-01-02 23:43:44 +0000

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -78,7 +78,7 @@ const Projects = () => {
                 </Tab.Pane>
 
                 <Tab.Pane eventKey="second">
-                  <p>An interactive web application that fosters collaboration and knowledge-sharing within the geo-tech engineering community. It features user-friendly content creation and added search functionality that is tailored to the needs of industry professionals and wider enthusiasts. Tech Stack: Next.js, React, TypeScript, Tailwind CSS, NextAuth.js, Prisma, PostgreSQL database hosted on Railway, Vercel Cloud Deployment</p>
+                  <p>An interactive web application that fosters collaboration and knowledge-sharing within the geo-tech engineering community. It features user-friendly content creation and added search functionality that is tailored to the needs of industry professionals and wider enthusiasts. <br/> Tech Stack: Next.js, React, TypeScript, Tailwind CSS, NextAuth.js, Prisma, PostgreSQL database hosted on Railway, Vercel Cloud Deployment</p>
                   <Row className="nav-pills mb-5 justify-content-center align-items-center">
                     {projects
                       .filter((project) => project.title === "Geo-Tech Blog App")
